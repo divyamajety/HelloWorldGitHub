5f723b64d8a619952d515c129a7442cf6a391140
divyamajety <divya_majeti@gmail.com>
2015-02-03 17:52:12 -0500

    updated comments

--- src/com/divya/demo/HelloWorld.java
+++ src/com/divya/demo/HelloWorld.java
@@ -3,7 +3,8 @@ package com.divya.demo;
 public class HelloWorld {
 	
       public static void main(String args[]) {
-    	   //just adding comments
+    	  
+    	   //just added comments
            System.out.println("In main");
       }
 
