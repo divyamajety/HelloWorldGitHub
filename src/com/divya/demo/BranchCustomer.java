8cef26121a12de9f36ac01a31b052f255aad6034
divyamajety <divya_majeti@gmail.com>
2015-02-03 17:40:06 -0500

    added getters and setters to BranchCustomer in r2_customer

--- src/com/divya/demo/BranchCustomer.java
+++ src/com/divya/demo/BranchCustomer.java
@@ -2,4 +2,20 @@ package com.divya.demo;
 
 public class BranchCustomer {
 
+	private String custId;
+	private String custName;
+	
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public String getCustName() {
+		return custName;
+	}
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}	
+	
 }
